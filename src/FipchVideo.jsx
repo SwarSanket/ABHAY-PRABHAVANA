467409fa73258cc0d90e08c37e8f464bd919110a
imac <imac@imacs-iMac.local>
2024-08-01 11:38:13 +0530

    Initial commit

--- src/FipchVideo.jsx
+++ src/FipchVideo.jsx
@@ -1,15 +1,16 @@
+
 import React from "react";
 import FipchVdo from "./assets/fipch-vdo.mp4";
 
  export default function FipchVideo() {
     return (
       <div className="relative z-10 w-full max-h-1080px">
-        <video className="w-full h-[1080px] object-cover" muted autoPlay loop>
+        <video className="w-full h-[500px] sm:h-[600px] md:h-[700px] lg:h-[800px] xl:h-[1080px] object-cover" muted autoPlay loop>
           <source src={FipchVdo} type="video/mp4" />
         </video>
-        <div className="absolute top-0 left-0 w-full h-[1080px] bg-black opacity-20"></div>
+        <div className="absolute top-0 left-0 w-full h-[500px] sm:h-[600px] md:h-[700px] lg:h-[800px] xl:h-[1080px] bg-black opacity-20"></div>
         <div className="absolute w-full p-4 text-center bottom-20">
-          <span className="font-semibold text-white text-[50px]">Timeless Jain Values</span>
+          <span className="font-semibold text-white  text-[25px] sm:text-[40px] md:text-[50px]">Timeless Jain Values</span>
         </div>
       </div>
     );
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: '/FIPCH/', // base path for GitHub Pages
+  base: mode === 'development' ? '/' : '/FIPCH/', // Use '/' in development and '/FIPCH/' in production
   build: {
     outDir: 'dist'
   }
-})
+}))
